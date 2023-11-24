6fa50b8fd08a215574f32fe2979be2a0b7d0c8c6
Daniel Hjartland <squashical@gmail.com>
2023-11-24 14:19:03 +0100

    feat: base command for clipping
    Simple utility for converting clipboard content to markdown

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,45 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/SQUASHD/go-clip2md/internal/clippy"
+	"github.com/SQUASHD/go-clip2md/internal/converter"
+	"github.com/SQUASHD/go-clip2md/internal/postprocess"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-clip2md",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "go-clip2md is a tool to convert clipboard content to markdown",
+	Long:  `go-clip2md is a tool to convert clipboard content to markdown`,
+	Run: func(cmd *cobra.Command, args []string) {
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		content, err := clippy.ReadFromClipboard()
+		if err != nil {
+			fmt.Println("Error reading from clipboard")
+			return
+		}
+
+		language, _ := cmd.Flags().GetString("lang")
+		domain, _ := cmd.Flags().GetString("domain")
+		md, err := converter.ConvertHTML2Md(domain, content)
+		if err != nil {
+			fmt.Println("Error converting to markdown")
+			return
+		}
+		md = postprocess.PostProcessMarkdown(md, language)
+
+		if err = clippy.WriteToClipboard(md); err != nil {
+			fmt.Println("Error writing to clipboard")
+			return
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,15 +52,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-clip2md.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringP("lang", "l", "go", "Language to use for syntax highlighting")
+	rootCmd.PersistentFlags().StringP("domain", "d", "", "Domain to use for relative links, e.g. https://en.wikipedia.org")
 }
-
-
