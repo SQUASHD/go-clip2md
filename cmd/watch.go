16e64d6764988154416e8824b7276148017b0c38
Daniel Hjartland <squashical@gmail.com>
2023-11-24 14:19:34 +0100

    feat: watch clipboard
    Watch the clipboard and write to an out directory, either by appending
    or writing a new file.
    
    Append mode: creates a new file for the first copied content, then
    appends subsequent content to the same file
    New mode: the default mode creates a new file each time the clipboard
    content changes
    
    Flags
    --domain: process image tags with relative links by prepending the
    domain
    --language: convert code blocks that are written in a language to the
    chosen langauge. Often times the code block will contain a nonsense
    language
    --interval: how often the clipboard content is checked
    --out: what directory to write files to. Default current directory

--- cmd/watch.go
+++ cmd/watch.go
@@ -1,40 +1,151 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"time"
 
+	"github.com/SQUASHD/go-clip2md/internal/clippy"
+	"github.com/SQUASHD/go-clip2md/internal/converter"
+	gen "github.com/SQUASHD/go-clip2md/internal/generator"
+	"github.com/SQUASHD/go-clip2md/internal/postprocess"
+	"github.com/SQUASHD/go-clip2md/internal/sanitize"
 	"github.com/spf13/cobra"
 )
 
-// watchCmd represents the watch command
 var watchCmd = &cobra.Command{
 	Use:   "watch",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Watch clipboard for changes",
+	Long:  `Watch clipboard for changes and generate markdown files`,
+	Run:   runWatchCmd,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("watch called")
-	},
+func runWatchCmd(cmd *cobra.Command, args []string) {
+	mode, file, count := setupWatch(cmd)
+	var lastContent string
+
+	lastContent, _ = clippy.ReadFromClipboard()
+	for {
+		contentChanged, currentContent := checkClipboardChange(&lastContent)
+		if contentChanged {
+			var err error
+			file, err = processClipboardContent(currentContent, &count, mode, file, cmd)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+		}
+		time.Sleep(time.Duration(getInterval(cmd)) * time.Second)
+	}
+
+	if file != nil {
+		file.Close()
+	}
+}
+
+func setupWatch(cmd *cobra.Command) (string, *os.File, int) {
+	out, _ := cmd.Flags().GetString("out")
+	mode, _ := cmd.Flags().GetString("mode")
+
+	if mode != "new" && mode != "append" {
+		fmt.Println("Invalid mode:", mode)
+		os.Exit(1)
+	}
+
+	if err := gen.SetWorkingDirectory(out); err != nil {
+		fmt.Println("Error setting working directory:", err)
+		os.Exit(1)
+	}
+
+	return mode, nil, 0
+}
+
+func checkClipboardChange(lastContent *string) (bool, string) {
+	currentContent, err := clippy.ReadFromClipboard()
+	if err != nil {
+		fmt.Println("Error reading clipboard:", err)
+		return false, *lastContent
+	}
+	if currentContent != *lastContent {
+		*lastContent = currentContent
+		return true, currentContent
+	}
+	return false, *lastContent
+}
+
+func processClipboardContent(content string, count *int, mode string, file *os.File, cmd *cobra.Command) (*os.File, error) {
+	*count++
+	md, err := convertContentToMarkdown(content, cmd)
+	if err != nil {
+		return file, fmt.Errorf("error converting HTML: %w", err)
+	}
+
+	filepath := generateFilePath(md, *count, cmd)
+	if mode == "new" || *count == 1 {
+		if file != nil {
+			file.Close()
+		}
+		file, err = openFileForWrite(filepath, mode)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err := writeFile(file, md); err != nil {
+		return file, err
+	}
+
+	if mode == "new" || *count == 1 {
+		fmt.Println("New file:", filepath)
+	} else {
+		fmt.Println("Appended to file")
+	}
+	return file, nil
+}
+
+func convertContentToMarkdown(content string, cmd *cobra.Command) (string, error) {
+	lang, _ := cmd.Flags().GetString("lang")
+	domain, _ := cmd.Flags().GetString("domain")
+	md, err := converter.ConvertHTML2Md(domain, content)
+	if err != nil {
+		return "", fmt.Errorf("error converting HTML: %w", err)
+	}
+	return postprocess.PostProcessMarkdown(md, lang), nil
+}
+
+func generateFilePath(md string, count int, cmd *cobra.Command) string {
+	pattern, _ := cmd.Flags().GetString("pattern")
+	h1 := postprocess.ExtractFirstHeader(md)
+
+	if pattern != "" {
+		return gen.GenerateFilepath(fmt.Sprintf("%s%d-", pattern, count), h1) + ".md"
+	}
+	return sanitize.ConvertHeaderToFilePath(h1) + ".md"
+}
+
+func openFileForWrite(filepath string, mode string) (*os.File, error) {
+	if mode == "new" {
+		return os.Create(filepath)
+	}
+	return os.OpenFile(filepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+}
+
+func writeFile(file *os.File, content string) error {
+	if _, err := file.WriteString(content + "\n"); err != nil {
+		return fmt.Errorf("error writing to file: %w", err)
+	}
+	return nil
+}
+
+func getInterval(cmd *cobra.Command) int {
+	interval, _ := cmd.Flags().GetInt("interval")
+	return interval
 }
 
 func init() {
 	rootCmd.AddCommand(watchCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// watchCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// watchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	watchCmd.Flags().StringP("out", "o", ".", "Output directory")
+	watchCmd.Flags().IntP("interval", "i", 1, "Interval to check clipboard for changes")
+	watchCmd.Flags().StringP("pattern", "p", "", "Pattern to use for file naming, e.g. doc, which becomes doc1.md, doc2.md, etc.")
+	watchCmd.Flags().StringP("mode", "m", "new", "mode for file writing, new or append")
 }
